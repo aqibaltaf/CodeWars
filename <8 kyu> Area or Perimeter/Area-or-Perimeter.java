0e0497bf455a92093f4106b6615e5f0e93f26285
Aqib Altaf <61707240+aqibaltaf@users.noreply.github.com>
2022-02-06 14:10:55 +0500

    Create Area-or-Perimeter.java

---
+++ <8 kyu> Area or Perimeter/Area-or-Perimeter.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static int areaOrPerimeter (int l, int w) {
+        // code away...
+      if(l == w){
+        return l*w;
+      }
+      return (l+w)*2;
+    }
+}
