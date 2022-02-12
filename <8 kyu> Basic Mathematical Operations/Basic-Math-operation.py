716219522cf9b4cf4e2b310fadd8ee2639c9eafa
Aqib Altaf <61707240+aqibaltaf@users.noreply.github.com>
2022-02-12 23:05:53 +0500

    Create Basic-Math-operation.py

---
+++ <8 kyu> Basic Mathematical Operations/Basic-Math-operation.py
@@ -0,0 +1,8 @@
+def basic_op(operator, value1, value2):
+    if operator == '+':
+        return value1 + value2
+    elif operator == '-':
+        return value1 - value2
+    elif operator == '*':
+        return value1 * value2
+    return value1 / value2
